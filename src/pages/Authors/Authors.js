fcf60186f28d284b750a6bc190e9f201282761ee
Muhammad Salimov <salimovmuhammad2322@gmail.com>
2023-03-25 19:47:17 +0500

    finish

--- src/pages/Authors/Authors.js
+++ src/pages/Authors/Authors.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import Css from './Authors.module.css'
 const Authors = () => {
-  return (
-    <div>Authors</div>
+  return (<section>
+        <div className={Css.container}>
+            <div className={Css.title}>
+               <b className={Css.h111}> TUZUVCHILAR HAQIDA MA'LUMOT</b>
+            </div>
+            <div className={Css.dflex}>
+                <div className={Css.col1}>
+                    <img src="./Autors.jpg" alt="img" />
+                </div>
+                <div className={Css.col2}>
+                   <div>
+                    <b className={Css.h222}>
+                        USAROV SIROJIDDIN RAXMATULLAYEVICH
+                    </b>
+                    <p className={Css.ppp}>
+                        Sharof Rashidov nomidagi Samarqand davlat universiteti, Tarix fakulteti, "Jahon tarixi" kafedrasi o'qituvchisi, tarix fanlari bo'yicha falsafa doktori.
+                    </p>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  
+
+    
   )
 }
 
